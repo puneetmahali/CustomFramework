829d12150ffd6f363daa35e214e3b82200778d0c
Puneet Mahali <iospuneet@gmail.com>
2020-07-15 14:32:21 +0200

    Framework Basics done with Custom Framework testing app.

---
+++ CustomFramework/CustomFramework/RandomGenerator.swift
@@ -0,0 +1,22 @@
+//
+//  RandomGenerator.swift
+//  CustomFramework
+//
+//  Created by Puneet Mahali on 15.07.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import Foundation
+
+public class RandomGenerator {
+    private init() {}
+    
+    public static func string() -> String {
+        return UUID().uuidString
+    }
+    
+    public static func integer() -> Int {
+        return Int(arc4random())
+    }
+    
+}
